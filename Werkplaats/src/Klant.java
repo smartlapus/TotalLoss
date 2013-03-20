087d4552f60405cd0013c4e43e669aff3ba21bab
Dion <dion139@hotmail.com>
2013-03-20 12:44:59 +0100

    Non GUI klasse met attributen en lege methode

---
+++ Werkplaats/src/Klant.java
@@ -0,0 +1,13 @@
+
+public class Klant {
+	
+	private String naam;
+	private String adres;
+	private String geboortedatum;
+	
+	public Klant(String nm, String adr, String gebdatum){
+	naam = nm;
+	adres = adr;
+	geboortedatum = gebdatum;
+	}
+}
