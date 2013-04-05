beb6e457fe6fddbb83994c543164f23a653ceff5
Samme Kleijn <sammedavid@msn.com>
2013-04-05 15:09:10 +0200

    VoorraadOverzichtFrame layout gemaakt + BezettingOpvragen frame aangepast
    Opzet van de layout gemaakt

--- Werkplaats/src/BezettingOpvragenFrame.java
+++ Werkplaats/src/BezettingOpvragenFrame.java
@@ -8,17 +8,18 @@ public class BezettingOpvragenFrame extends Hoofdmenu implements ActionListener 
 	// ArrayLists voor de dagen, maanden en jaren
 	private static String[] dag = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", 
 		"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
-	private static String[] maand = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+	private static String[] maand = {"Januari", "Februari", "Maart", "April", "Mei", "Juni", 
+		"Juli", "Augustus", "September", "Oktober", "November", "December"};
 	private static String[] jaar = {"2011", "2012", "2013"};
 	
 	//ComboBox wordt gevuld met de inhoud van de ArrayList(s)
 	private JComboBox boxDag = new JComboBox(dag);
 	private JComboBox boxMaand = new JComboBox(maand);
 	private JComboBox boxJaar = new JComboBox(jaar);
-	private JLabel lDag = new JLabel("dag"); 
-	private JLabel lMaand = new JLabel("maand"); 
-	private JLabel lJaar = new JLabel("jaar"); 
-	private JButton Opvragen = new JButton("Opvragen");
+	JButton Opvragen;
+	JLabel lDag, lMaand, lJaar;
+	
+	
 	public BezettingOpvragenFrame(Bedrijf b)
 	{
 		super(b);
@@ -28,9 +29,9 @@ public class BezettingOpvragenFrame extends Hoofdmenu implements ActionListener 
 		cp.setBackground(UIManager.getColor("control"));
 		GridBagConstraints c = new GridBagConstraints();
 
-
 		//Labels, Comboboxen en button worden toegevoegd aan de container
-		cp.add(boxDag);cp.add(boxMaand);cp.add(boxJaar);
+		
+		
 		lDag = new JLabel("Dag");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridy = 2;
@@ -44,24 +45,13 @@ public class BezettingOpvragenFrame extends Hoofdmenu implements ActionListener 
 		c.fill = GridBagConstraints.HORIZONTAL;
 		cp.add(lJaar, c);
 		
+		cp.add(boxDag);cp.add(boxMaand);cp.add(boxJaar);
+		
 		Opvragen = new JButton("Opvragen");
 		c.weightx = 0.0;
 		c.gridwidth = 0;
 		c.gridx = 0;
 		c.gridy = 4;
 		cp.add(Opvragen, c);
-
-		c.gridx = 1;
-		c.gridy = GridBagConstraints.RELATIVE;
-		c.gridwidth = 1;
-		c.gridheight = 2;
-		c.insets = new Insets(2, 2, 2, 2);
-		c.anchor = GridBagConstraints.EAST;
-		
-		//Labels, Comboboxen en button worden toegevoegd aan de container
-		cp.add(lDag); cp.add(lMaand);cp.add(lJaar);
-		cp.add(boxDag);cp.add(boxMaand);cp.add(boxJaar);
-		cp.add(Opvragen);
-
 	}
 }
